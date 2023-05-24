eec48e3b5331dea503e9915078fb8b4b4f46faa3
Jared Sewak <jaredsewak@gmail.com>
2023-05-24 15:09:36 -0600

    created server file and html files

--- crud.py
+++ crud.py
@@ -9,10 +9,26 @@ def create_user(email, password):
     new_user = User(email=email, password=password)
     return new_user
 
+def get_users():
+    """Gets all users"""
+    return User.query.all()
+
+def get_user_by_id(user_id):
+    """Gets user by id"""
+    return User.query.get(user_id)
+
 def create_movie(title, overview, release_date, poster_path):
     """Creates a new movie object and returns it"""
     new_movie = Movie(title=title, overview=overview, release_date=release_date, poster_path=poster_path)
     return new_movie
+
+def get_movies(): #read
+    """Return all movies from database"""
+    return Movie.query.all()
+
+def get_movie_by_id(movie_id):
+    """Gets movie by id"""
+    return Movie.query.get(movie_id)
 
 def create_rating(user, movie, score):
     """Creates a new rating object and returns it"""     
